f655f2f14a365c4cedc133518ac7c4db28bea170
luoshuitaotao <jcui@unomaha.edu>
2020-10-02 23:51:42 -0500

    properties

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -9,4 +9,25 @@ var userOne= {
     }
 };
 
-console.log(userOne.name)
+var userTwo= {
+    email:'yoshi@ninjas.com',
+    name:'Yoshi',
+    login(){
+        console.log(this.email,'has logged in')
+    },
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+};
+var userThree= {
+    email:'leini@ninjas.com',
+    name:'leini',
+    login(){
+        console.log(this.email,'has logged in')
+    },
+    logout(){
+        console.log(this.email, 'has logged out')
+    }
+};
+userOne.name = 'Yoshi';
+            
