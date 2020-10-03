64354cd106052a95e8d64ee200a428efebf565c7
luoshuitaotao <jcui@unomaha.edu>
2020-10-03 18:14:50 -0500

    method chaining

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -2,16 +2,25 @@ class User {
     constructor(email, name){
         this.email = email;
         this.name= name;
+        this.score = 0;
     }
     login(){
         console.log(this.email, 'just logged in');
+        return this;
     } 
     logout(){
-        console.log(this.name, 'just logged out')
+        console.log(this.name, 'just logged out');
+        return this;
     }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    } 
 }
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mario.com', 'Yoshi');
 
-userOne.login();
-userTwo.logout();
+userOne.login().updateScore().updateScore().updateScore().logout();
+
+console.log(userOne);
