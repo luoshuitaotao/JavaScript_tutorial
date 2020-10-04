aa58975ffd6e95e0cf9a22f6d5f38ef810b75085
luoshuitaotao <jcui@unomaha.edu>
2020-10-04 18:46:04 -0500

    understand prototype inheritance

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -12,8 +12,21 @@ User.prototype.logout = function(){
     console.log(this.email, 'has logout');
 }
 
+function Admin (...args){
+    User.apply(this, args)
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    })
+};
+
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mario.com', 'Yoshi');
-    
-console.log(userOne);
-userTwo.login();
+var admin = new Admin('shaun@ininjas.com', 'Shaun'); 
+var users = [userOne, userTwo, admin];
+
+console.log(admin);
