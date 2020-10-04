5012e46d6390f96b4a5392a7a3b07d4f87bd6e1b
luoshuitaotao <jcui@unomaha.edu>
2020-10-04 18:17:50 -0500

    understand prototype

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -2,10 +2,16 @@ function User(email,name){
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login =  function(){
-        console.log(this.email, 'has login');
-    }
 }
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has login');
+}
+User.prototype.logout = function(){
+    this.online = true;
+    console.log(this.email, 'has logout');
+}
+
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mario.com', 'Yoshi');
     
