3055f008bcdf9b48a02305a7a1d8f3ad4d1a1a66
luoshuitaotao <jcui@unomaha.edu>
2020-10-04 00:22:05 -0500

    constructors

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -1,37 +1,13 @@
-class User {
-    constructor(email, name){
-        this.email = email;
-        this.name= name;
-        this.score = 0;
-    }
-    login(){
-        console.log(this.email, 'just logged in');
-        return this;
-    } 
-    logout(){
-        console.log(this.name, 'just logged out');
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, 'score is now', this.score);
-        return this;
-    } 
-}
-class Admin extends User {
-    deleteUser(user){
-        users= users.filter(u =>{
-            return u.email!= user.email
-        })
-        
+function User(email,name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+    this.login =  function(){
+        console.log(this.email, 'has login');
     }
 }
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mario.com', 'Yoshi');
-var admin = new Admin('shaun@nijas.com', 'shaun');
-
-
-var users= [userOne, userTwo, admin]
-admin.deleteUser(userOne);
-
-console.log(users)
+    
+console.log(userOne);
+userTwo.login();
