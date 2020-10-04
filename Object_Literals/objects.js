b6ff5cdd97ecd78be02461e07169fb065a5bd064
luoshuitaotao <jcui@unomaha.edu>
2020-10-03 20:23:13 -0500

    class inheritance

--- Object_Literals/objects.js
+++ Object_Literals/objects.js
@@ -18,9 +18,20 @@ class User {
         return this;
     } 
 }
+class Admin extends User {
+    deleteUser(user){
+        users= users.filter(u =>{
+            return u.email!= user.email
+        })
+        
+    }
+}
 var userOne = new User('ryu@ninjas.com','Ryu');
 var userTwo = new User('yoshi@mario.com', 'Yoshi');
+var admin = new Admin('shaun@nijas.com', 'shaun');
 
-userOne.login().updateScore().updateScore().updateScore().logout();
 
-console.log(userOne);
+var users= [userOne, userTwo, admin]
+admin.deleteUser(userOne);
+
+console.log(users)
